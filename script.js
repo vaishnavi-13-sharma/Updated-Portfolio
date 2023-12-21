d43999ed4cd8325d6fe5afa5a3f86679e3e3e63d
nav <fpd16@txstate.edu>
2023-12-21 14:01:23 -0600

    stylesheet, script

---
+++ script.js
@@ -0,0 +1,37 @@
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        section.style.display = 'none';
+    });
+
+    const targetSection = document.getElementById(sectionId);
+    targetSection.style.display = 'flex';
+
+    window.scrollTo({
+        top: targetSection.offsetTop,
+        behavior: 'smooth'
+    });
+
+    closeNav();
+}
+
+function toggleNav() {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('show');
+}
+
+function closeNav() {
+    const nav = document.querySelector('nav');
+    nav.classList.remove('show');
+}
+
+let prevScrollPos = window.pageYOffset;
+window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollPos > currentScrollPos) {
+        document.querySelector('header').style.top = '0';
+    } else {
+        document.querySelector('header').style.top = '-70px';
+    }
+    prevScrollPos = currentScrollPos;
+};
